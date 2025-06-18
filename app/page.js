b86d547927c80e93c1eb3fa1eab59a84b5c78ee0
Seb-mlrd <sebmlrd06@gmail.com>
2025-06-18 02:17:24 +0200

    ajout des boutons sur la home

--- app/page.js
+++ app/page.js
@@ -1,95 +1,30 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+
+import { useAuth } from '../context/AuthContext';
+import LogoutButton from "../components/LogoutButton";
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+    const { user, loading } = useAuth();
+    const router = useRouter();
+    if (loading) return <p>Chargement...</p>;
+  
+    return (
+      <div>
+        <h1>Bienvenue {user && user.nickname}</h1>
+        {user?.is_admin && <p>Vous êtes administrateur 🎉</p>}
+        {user ?
+          <LogoutButton />
+        :
+        <div>
+          <button onClick={() => router.push('/register')} style={{ padding: 10, marginInline: 20, marginTop: 20, cursor: 'pointer' }}>
+            Inscription
+          </button>
+          <button onClick={() => router.push('/login')} style={{ padding: 10, marginInline: 20, marginTop: 20, cursor: 'pointer' }}>
+            Connexion
+          </button>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+      }
+      </div>
+    );
 }
--- components/LogoutButton.js
+++ components/LogoutButton.js
@@ -9,7 +9,7 @@ export default function LogoutButton() {
 
   const handleLogout = async () => {
     await logout();
-    router.push('/login');
+    router.push('/');
   };
 
   return (
